67ec928704b404f254e68cc09b69182c07ca3b46
William (Wgyt) Horning <github@wgyt.tk>
2021-02-18 13:04:43 +0000

    edit error page

--- index.js
+++ index.js
@@ -1,16 +1,23 @@
 const express = require('express');
 const ejs = require('ejs');
 const app = express();
-function error(host,res,req,errornumber,errordesc){
+app.set("view engine", "ejs");
+app.set("views", __dirname + "/views");
+app.set("view options", { layout: true } );
+const version = "0.1-alpha"
+function error(host,res,req,errornumber,errordesc,version){
 	res.status(errornumber);
+	date = Date.now() * 1000
+	date2 = new Date(date)
+	date3 = date2.toLocaleString("en-US", {timeZoneName: "short"})
 	console.log(`${errornumber} ${errordesc} error @ https://${host}${req.url}`)
-	ejs.renderFile( `${__dirname}/views/all/error.html`, {errornumber: errornumber,errordesc:errordesc,url:`https://${host}${req.url}`,host:host||wgytdomains}, function(err, str) { res.send(str) });
+	res.render(`all/error`,{errornumber: errornumber,errordesc:errordesc,url:`https://${host}${req.url}`,host:host||wgytdomains,timestamp:date3,version:version});
 }
 function servePage(host,res,req,error,sites){
 	if (sites.includes(host)===true){
 		res.send('this is WIP')
 	}else{
-		error(host,res,req,501,'Not Implemented <br> This Site Doesn\'t exist.')
+		error(host,res,req,501,'Not Implemented. This Site Doesn\'t exist.',version)
 	}
 }
 app.get('*', (req, res) => {
