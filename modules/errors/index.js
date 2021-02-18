c7c940b49f1cb0a568e1148023679cc91fa4cd7a
William (Wgyt) Horning <github@wgyt.tk>
2021-02-18 18:50:09 +0000

    small fixes

--- modules/errors/index.js
+++ modules/errors/index.js
@@ -4,5 +4,5 @@ module.exports = function(host,res,req,errornumber,errordesc,version,ejs){
 	date2 = new Date(date)
 	date3 = date2.toLocaleString("en-US", {timeZoneName: "short",timeZone: "America/New_York"})
 	console.log(`${errornumber} ${errordesc} error @ https://${host}${req.url}`)
-	ejs.renderFile('modules/errors/index.ejs', {errornumber: errornumber,errordesc:errordesc,url:`https://${host}${req.url}`,host:host||wgytdomains,timestamp:date3,version:version}, {}, function (err, template) { if (err) { throw err; } else { res.end(template); } }); 
+	ejs.renderFile('modules/errors/index.ejs', {errornumber: errornumber,errordesc:errordesc,url:`https://${host}${req.url}`,host:host||wgytcraft,timestamp:date3,version:version}, {}, function (err, template) { if (err) { throw err; } else { res.end(template); } }); 
 }
