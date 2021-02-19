a3d28b5fe1933c8abe5ce73f34e81c2ac700ec6d
William (Wgyt) Horning <github@wgyt.tk>
2021-02-19 16:49:52 +0000

    module imports work for error modules!

--- index.js
+++ index.js
@@ -1,29 +1,28 @@
 const express = require('express');
 const ejs = require('ejs');
+const fs = require('fs');
 const app = express();
+const Git = require("nodegit");
+modules = JSON.parse(fs.readFileSync('modules/modules.json'))
 app.set("view engine", "ejs");
-app.set("view options", { layout: true } );
+app.set("view options", { layout: true });
 const version = "0.1-alpha"
-function error(host,res,req,errornumber,errordesc,version,ejs){
-	res.status(errornumber);
-	date = Date.now() * 1000
-	date2 = new Date(date)
-	date3 = date2.toLocaleString("en-US", {timeZoneName: "short",timeZone: "America/New_York"})
-	console.log(`${errornumber} ${errordesc} error @ https://${host}${req.url}`)
-	ejs.renderFile('modules/errors/index.ejs', {errornumber: errornumber,errordesc:errordesc,url:`https://${host}${req.url}`,host:host||wgytcraft,timestamp:date3,version:version}, {}, function (err, template) { if (err) { throw err; } else { res.end(template); } }); 
-}
-function servesite(host,res,req,error,sites){
-	if (sites.includes(host)===true){
+for (module of modules.moduleList) {
+Git.Clone(`https://github.com/wgytcraft/${module}`, `./modules/${module}`).then(function(repository) {
+});}
+const error = require(`${__dirname}/modules/${modules.errorHandler}/index.js`)
+function servesite(host, res, req, error, sites) {
+	if (sites.includes(host) === true) {
 		res.send('this is WIP') // comment this out
 		// servepage(host,res,req)
-	}else{
-		error(host,res,req,501,'Not Implemented. This Site Doesn\'t exist.',version,ejs)
+	} else {
+		error(host, res, req, 501, 'Not Implemented. This Site Doesn\'t exist.', version, ejs)
 	}
 }
 app.get('*', (req, res) => {
 	host = req.get('host')
-	servesite(host,res,req,error,process.env.sites)
+	servesite(host, res, req, error, process.env.sites)
 });
 app.listen(3000, () => {
-  console.log('server started');
+	console.log('server started');
 });
--- modules/errors/readme.md
+++
@@ -1,4 +0,0 @@
-# errors
-## beautiful tailwind error pages, powered by ejs, inspired by wgyt.tk, shipped with wgytcraft, by default
-How to install:
-- in your modules.json file add "github.com/wgytcraft/errors.git" to the end of the moduleList array, you might need to add a comma.
